978678e47db2e0da1dacb22b0e39de8d7eff2abd
Pawel Urbanczyk <pawel.urbanczyk90@gmail.com>
2018-01-01 21:04:14 +0100

    Dodanie plików konfiguracyjnych i polaczenia z baza danych

---
+++ save.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+
+    <meta charset="utf-8">
+    <title>Zapisanie się do newslettera</title>
+    <meta name="description" content="Używanie PDO - zapis do bazy MySQL">
+    <meta name="keywords" content="php, kurs, PDO, połączenie, MySQL">
+
+    <meta http-equiv="X-Ua-Compatible" content="IE=edge">
+
+    <link rel="stylesheet" href="main.css">
+    <link href="https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Newsletter</h1>
+        </header>
+    </div>
+
+    <main>
+        <article>
+            <p>Pomyślnie twój Email został do listy newslettera!</p>
+        </article>
+    </main>
+</body>
+</html>
