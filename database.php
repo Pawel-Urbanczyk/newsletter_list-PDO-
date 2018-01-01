e3e0661213759683bbd39b75f11b29f5f44993d8
Pawel Urbanczyk <pawel.urbanczyk90@gmail.com>
2018-01-01 21:45:07 +0100

    Dodanie walidacji i insertu do bazy danych

--- database.php
+++ database.php
@@ -3,11 +3,13 @@
 $config = require_once 'config.php';
 
 try{
-    $db = new PDO("mysql:host={$config['host']};dbname={$config['database']};charset=utf-8", $config['user'], $config['password'], [
+    $db = new PDO("mysql:host={$config['host']};dbname={$config['database']};charset=utf8", $config['user'], $config['password'], [
         PDO::ATTR_EMULATE_PREPARES => false,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
+
 }catch(PDOException $error){
+   //echo $error->getMessage();
     exit('Database error');
 }
 ?>
--- save.php
+++ save.php
@@ -1,4 +1,26 @@
 <?php
+session_start();
+require_once 'database.php';
+
+if(isset($_POST['email'])){
+
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+
+
+    if(empty($email)){
+        $_SESSION['given_email'] = $_POST['email'];
+        header('Location: index.php');
+        }else{
+            $query = $db->prepare('INSERT INTO USERS VALUES (NULL, :email)');
+            $query->bindValue(':email', $email, PDO::PARAM_STR);
+            $query-> execute();
+        }
+
+    }else{
+
+        header('Location: index.php');
+        exit();
+}
 
 ?>
 <!DOCTYPE html>
@@ -14,9 +36,9 @@
 
     <link rel="stylesheet" href="main.css">
     <link href="https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <![endif]-->
+    <!--[if lt IE 9]
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>!
+    [endif]-->
 
 </head>
 <body>
